a523a28b31227209f460511e023f04bf92adc8c8
ruslan <ruslanhu05@gmail.com>
2026-02-03 16:45:00 +0500

    Add product and storage models with initial image set

---
+++ Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopApp.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public string? Image1 { get; set; }
+
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+}
